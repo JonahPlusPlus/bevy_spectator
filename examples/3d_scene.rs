fed5bbd899ce82cb5c9ebae445505d150fb6df76
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2022-11-14 01:37:44 -0500

    Update 3d_scene.rs
    Was commented out temporarily

--- examples/3d_scene.rs
+++ examples/3d_scene.rs
@@ -47,7 +47,7 @@ fn setup(
     // camera
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-1.0, 1.5, 1.0),//.looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-1.0, 1.5, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         }, Spectator
     ));
