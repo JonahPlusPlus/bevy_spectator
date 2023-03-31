ead5445fa415381553a1782f5629e1babb268626
Maiko van der Veen <maikovdveen@gmail.com>
2023-03-31 23:44:24 +0200

    Added control scheme for 2d scenes
    Added 'orthographic' option to SpectatorSettings. Disables mouse looking & capturing. Makes W and S control y-axis movement.

---
+++ examples/2d_scene.rs
@@ -0,0 +1,25 @@
+use bevy::prelude::*;
+use bevy_spectator::*;
+
+fn main() {
+    App::new()
+        .insert_resource(SpectatorSettings {
+            orthographic: true,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(SpectatorPlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((Camera2dBundle::default(), Spectator));
+    commands.spawn(SpriteBundle {
+        transform: Transform {
+            scale: Vec3::new(10.0, 10.0, 1.0),
+            ..default()
+        },
+        ..default()
+    });
+}
