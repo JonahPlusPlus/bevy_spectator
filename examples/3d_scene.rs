162b5cdc01502f1aa89bdf7d74e0b2538f70e7f1
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-02-23 20:19:58 +0200

    Adjust camera position

--- examples/3d_scene.rs
+++ examples/3d_scene.rs
@@ -46,7 +46,7 @@ fn setup(
     // camera
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-1.0, 1.5, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-3.0, 1.5, 3.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         },
         Spectator,
