d05245b8bcecdc3bab429414dae3eb162746e10b
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-12-07 02:33:57 +0200

    Remove useless comments

--- examples/3d_scene.rs
+++ examples/3d_scene.rs
@@ -21,18 +21,15 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // plane
     commands.spawn((
         Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
         MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
     ));
-    // cube
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::default())),
         MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
         Transform::from_xyz(0.0, 0.5, 0.0),
     ));
-    // light
     commands.spawn((
         PointLight {
             shadows_enabled: true,
@@ -40,7 +37,6 @@ fn setup(
         },
         Transform::from_xyz(4.0, 8.0, 4.0),
     ));
-    // camera
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(-3.0, 1.5, 3.0).looking_at(Vec3::ZERO, Vec3::Y),
